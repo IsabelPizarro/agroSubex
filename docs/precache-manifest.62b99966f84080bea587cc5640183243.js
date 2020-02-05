1ae5dadfc9f071774365006714b468db1a18c5e8
Isabel <chabelipizarro1@gmail.com>
2020-02-05 09:43:55 +0100

    change build to docs

---
+++ docs/precache-manifest.62b99966f84080bea587cc5640183243.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "81fab94d28a33df0e5f2",
+    "url": "./static/css/main.adf1e6f6.chunk.css"
+  },
+  {
+    "revision": "81fab94d28a33df0e5f2",
+    "url": "./static/js/main.81fab94d.chunk.js"
+  },
+  {
+    "revision": "703f22ae796a59138f74",
+    "url": "./static/js/1.703f22ae.chunk.js"
+  },
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "3418a419c5f5e85467c3bf2cb8ac7b4a",
+    "url": "./static/media/vacas.3418a419.jpg"
+  },
+  {
+    "revision": "26dc3fef463f62101452ddc11b4fa6d1",
+    "url": "./index.html"
+  }
+];
